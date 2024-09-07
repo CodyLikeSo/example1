861b243954add682100ec16465b3c3227d37b7f9
CodyLikeSo <codywantmoney@gmail.com>
2024-09-07 12:27:26 +0300

    v

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Navbar from './navigation/navbar'
+import HoverButton from './navigation/hoverbar'
+
+
+function App() {
+  return (
+    <>
+      <div className='text-6xl'>
+        <HoverButton />
+      </div>
+    </>
+  )
+}
+
+export default App
