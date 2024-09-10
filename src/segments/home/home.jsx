0a06f413119f6f858f5cbdb3dc954909548de000
CodyLikeSo <codywantmoney@gmail.com>
2024-09-10 16:03:26 +0300

    v

--- src/segments/home/home.jsx
+++ src/segments/home/home.jsx
@@ -1,45 +1,9 @@
 export const Home = () => (
-  <div className="text-center">
-    <h2 className="text-xl font-bold">Home screen</h2>
-    <p>Content Home</p>
+  <div>
+    <div className="p-4 bg-[#242424] rounded-2xl shadow-[0_0px_40px_10px_rgba(0,0,0,0.25)]">
+
+    </div>
   </div>
 );
 
 export default Home;
-
-// import React, { useState, useEffect } from 'react';
-
-// export const Home = () => {
-//   const [position, setPosition] = useState({ x: 0, y: 0 });
-
-//   useEffect(() => {
-//     const handleMouseMove = (event) => {
-//       const { clientX, clientY } = event;
-//       setPosition({ x: clientX, y: clientY });
-//     };
-
-//     window.addEventListener('mousemove', handleMouseMove);
-
-//     return () => {
-//       window.removeEventListener('mousemove', handleMouseMove);
-//     };
-//   }, []);
-
-//   const calculateTransform = () => {
-//     const offsetX = (position.x / window.innerWidth) * 10 - 5;
-//     const offsetY = (position.y / window.innerHeight) * 10 - 5;
-//     return `translate(${offsetX}px, ${offsetY}px)`;
-//   };
-
-//   return (
-//     <div
-//       className="text-center"
-//       style={{ transform: calculateTransform(), transition: 'transform 0.1s ease-out' }}
-//     >
-//       <h2 className="text-xl font-bold">Home screen</h2>
-//       <p>Content Home</p>
-//     </div>
-//   );
-// };
-
-// export default Home;
