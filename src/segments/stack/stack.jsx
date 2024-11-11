08ced3e27635d70a7561e138c2d1340bfd8e98b5
CodyLikeSo <codywantmoney@gmail.com>
2024-11-11 05:50:17 +0300

    v

--- src/segments/stack/stack.jsx
+++ src/segments/stack/stack.jsx
@@ -17,8 +17,8 @@ const technicalSkills = [
 
 export const Stack = () => (
   <div className="p-1">
-    <div className="flex rounded-[15px]">
-      <div className="w-1/3 pr-4 border-green-600 border-[1px] rounded-[15px] p-6 mr-4">
+    <div className="flex flex-col md:flex-row rounded-[15px] md:h-[515px]">
+      <div className="hidden md:block md:w-1/3 w-full pr-4 border-green-600 border-[1px] rounded-[15px] p-6 mr-4 md:max-h-[calc(80vh-150px)] md:overflow-y-auto">
         <h2 className="md:text-xl text-md font-bold mb-4 text-green-300">Project Management Skills:</h2>
         <ul className="list-disc text-sm md:text-inherit list-inside mb-6">
           {projectManagementSkills.map((skill, index) => (
@@ -33,12 +33,12 @@ export const Stack = () => (
           ))}
         </ul>
       </div>
-      <div className="w-2/3 border-[1px] p-6 border-green-600 rounded-[15px] pl-4">
-        {/* Здесь можно добавить контент для правой области */}
+      <div className=" md:w-2/3 border-[1px]  p-6 border-green-600 rounded-[15px] pl-4 md:max-h-[calc(80vh-150px)] md:overflow-y-auto md:h-[515px]">
         <h2 className="text-xl font-bold mb-4 text-green-300 text-center">Description</h2>
-        <p className="">This is a short list of my skilts. To learn more you can go to the DEVELOPMENT or MANAGEMENT page. Also, my resume can be found on Linkedin.</p>
-        <img src="src/assets/cat.png" className="absolute px-24 py-12"/>
+        <p className="">This is a short list of my skills. To learn more you can go to the DEVELOPMENT or MANAGEMENT page. Also, my resume can be found on LinkedIn.</p>
+        <img src="src/assets/cat2.png" className="hidden md:block py-[30%] ml-auto"/>
       </div>
     </div>
   </div>
 );
+
